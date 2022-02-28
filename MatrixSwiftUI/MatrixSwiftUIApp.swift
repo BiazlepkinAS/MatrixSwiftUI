ad3ac1203e26d4180c1c8091bca0069e557e0897
Davide Scheriani <davide@resecurity.com>
2022-02-28 17:28:20 +0300

    Initial Commit

---
+++ MatrixSwiftUI/MatrixSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixSwiftUIApp.swift
+//  MatrixSwiftUI
+//
+//  Created by Andrei Bezlepkin on 28.02.22.
+//
+
+import SwiftUI
+
+@main
+struct MatrixSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
